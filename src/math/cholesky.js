3011a194a74b465dc170ab641c76059ced16700d
vanderci curvelo jr <vandercijr@yahoo.com.br>
2021-01-04 11:32:32 -0300

    add decomposition && solver

---
+++ src/math/cholesky.js
@@ -0,0 +1,31 @@
+export const cholesky = input => {  
+  const n = input.length
+  let i,j,k, sum=0, p=[]
+
+  for (i=0;i<n;i++) {
+    for (j=i;j<n;j++) {
+      for (sum=input[i][j],k=i-1;k>=0;k--) sum -= input[i][k]*input[j][k]
+      if (i == j) {
+        if (sum <= 0.0) throw "cholesky composition failed"
+        p[i] = Math.sqrt(sum)
+      } else  a[j][i]=sum/p[i]
+    }
+  }
+
+  return {a: a, d: p}
+}
+
+const choleskySolver = (a, d, b) => {  
+  const n = a.length
+  let i,k, sum=0, x=[]
+
+  for (i=0;i<n;i++) {
+    for (sum=b[i],k=i-1;k>=0;k--) sum -= a[i][k]*x[k]
+    x[i]=sum/d[i]
+  }
+  console.log('x1 ', x)
+  for (i=n;i>=0;i--) {
+    for (sum=x[i],k=i+1;k<n;k++) sum -= a[k][i]*x[k]
+    x[i]=sum/d[i];
+  }
+}
