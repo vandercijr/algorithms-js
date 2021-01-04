c733141e40dceff039453adc8a154425ac9a7f6a
vanderci curvelo jr <vandercijr@yahoo.com.br>
2021-01-04 13:22:38 -0300

    refactoring functions

--- src/math/cholesky.js
+++ src/math/cholesky.js
@@ -6,34 +6,42 @@
  * Data: 30/11/2020
  *
  */
-export const decompose = input => {  
-  const n = input.length
-  let i,j,k, sum=0, p=[]
+export const decompose = (input) => {
+  const n = input.length;
+  let i,
+    j,
+    k,
+    sum = 0,
+    p = [];
 
-  for (i=0;i<n;i++) {
-    for (j=i;j<n;j++) {
-      for (sum=input[i][j],k=i-1;k>=0;k--) sum -= input[i][k]*input[j][k]
+  for (i = 0; i < n; i++) {
+    for (j = i; j < n; j++) {
+      for (sum = input[i][j], k = i - 1; k >= 0; k--)
+        sum -= input[i][k] * input[j][k];
       if (i == j) {
-        if (sum <= 0.0) throw "cholesky composition failed"
-        p[i] = Math.sqrt(sum)
-      } else  a[j][i]=sum/p[i]
+        if (sum <= 0.0) throw "cholesky composition failed";
+        p[i] = Math.sqrt(sum);
+      } else a[j][i] = sum / p[i];
     }
   }
 
-  return {output: input, d: p}
-}
+  return { output: input, d: p };
+};
 
-const solver = (input, d, b) => {  
-  const n = input.length
-  let i,k, sum=0, x=[]
+export const solver = (input, d, b) => {
+  const n = input.length;
+  let i,
+    k,
+    sum = 0,
+    x = [];
 
-  for (i=0;i<n;i++) {
-    for (sum=b[i],k=i-1;k>=0;k--) sum -= input[i][k]*x[k]
-    x[i]=sum/d[i]
+  for (i = 0; i < n; i++) {
+    for (sum = b[i], k = i - 1; k >= 0; k--) sum -= input[i][k] * x[k];
+    x[i] = sum / d[i];
   }
 
-  for (i=n;i>=0;i--) {
-    for (sum=x[i],k=i+1;k<n;k++) sum -= input[k][i]*x[k]
-    x[i]=sum/d[i];
+  for (i = n; i >= 0; i--) {
+    for (sum = x[i], k = i + 1; k < n; k++) sum -= input[k][i] * x[k];
+    x[i] = sum / d[i];
   }
-}
+};
